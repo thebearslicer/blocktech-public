5d5408b27b418c66f0f4a91a8247a99d6e540db5
thebearslicer <70990720+thebearslicer@users.noreply.github.com>
2021-08-18 15:20:20 -0400

    started refactor

--- pycraft.py
+++ pycraft.py
@@ -1,5 +1,3 @@
-from inspect import currentframe
-from direct.showbase.PythonUtil import EnumIter
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from opensimplex import *
@@ -21,6 +19,53 @@ format_texture = load_texture("assets/format.png")
 
 block_to_place = 1
 
+class TerrainGenerator:
+    def create_random_heightmap(input_map):  # make sure input map is all zeros
+        for i in range(len(input_map)):
+            for j in range(len(input_map[i])):
+                input_map[i][j] = random.randint(0, 5)
+
+        return input_map  # literally random yup
+
+
+    def create_heightmap(size_z, size_x):
+        heightmap = [[0 for i in range(size_z)] for j in range(size_x)]  # ??? makes same thing as heightmap above
+
+        tmp = OpenSimplex(random.randint(10000, 99999))
+
+        multiplier = 0.001
+        for z in range(len(heightmap)):
+            for x in range(len(heightmap[z])):
+                # iterates over all positions in heightmap
+                heightmap[z][x] = (tmp.noise2d(z*multiplier, x*multiplier) + 1) **2
+                heightmap[z][x] = int(heightmap[z][x])
+
+        return heightmap
+
+
+    def generate_chunk(input_heightmap, top_texture, bottom_texture, fill, z_offset=0, x_offset=0):
+        for z in range(len(input_heightmap)):  # iterate over all lists in input_heightmap
+            for x in range(len(input_heightmap[z])):  # iterate over current sublist
+                grass_generate = Voxel(pos=(x + x_offset, input_heightmap[x][z], z + z_offset), given_texture=top_texture)  # create top layer
+                if fill:
+                    if input_heightmap[x][z] <= 0: continue  # check if top layer is at y<=0, if so, continue
+                    while input_heightmap[x][z] >= -1:  # check if top layer is not at y=0
+                        below = Voxel(pos=(x + x_offset, input_heightmap[x][z] - 1, z + z_offset),
+                                    given_texture=bottom_texture)  # this loop makes sure there aren't any weird gaps in generation
+                        input_heightmap[x][z] -= 1
+
+    def generate_tree(x, y, z):
+        trunk_bottom = Voxel(pos=(1 + x, 0 + y, z + 1), given_texture=wood_texture)
+        trunk_second = Voxel(pos=(1 + x, 1 + y, z + 1), given_texture=wood_texture)
+        trunk_third  = Voxel(pos=(1 + x, 2 + y, z + 1), given_texture=wood_texture)
+        trunk_forth  = Voxel(pos=(1 + x, 3 + y, z + 1), given_texture=wood_texture)
+        top_leaf     = Voxel(pos=(1 + x, 4 + y, z + 1), given_texture=leaves_texture)
+        left_leaf    = Voxel(pos=(0 + x, 3 + y, z + 1), given_texture=leaves_texture)
+        right_leaf   = Voxel(pos=(2 + x, 3 + y, z + 1), given_texture=leaves_texture)
+        behind_leaf  = Voxel(pos=(1 + x, 3 + y, z + 2), given_texture=leaves_texture)
+        forward_leaf = Voxel(pos=(1 + x, 3 + y, z + 0), given_texture=leaves_texture)
+    
+terrain = TerrainGenerator
 
 player = FirstPersonController()
 
@@ -112,54 +157,10 @@ heightmap = [
 ]
 
 
-def create_random_heightmap(input_map):  # make sure input map is all zeros
-    for i in range(len(input_map)):
-        for j in range(len(input_map[i])):
-            input_map[i][j] = random.randint(0, 5)
-
-    return input_map  # literally random yup
 
 
-def create_heightmap(size_z, size_x):
-    heightmap = [[0 for i in range(size_z)] for j in range(size_x)]  # ??? makes same thing as heightmap above
-
-    tmp = OpenSimplex(random.randint(10000, 99999))
-
-    multiplier = 0.001
-    for z in range(len(heightmap)):
-        for x in range(len(heightmap[z])):
-            # iterates over all positions in heightmap
-            heightmap[z][x] = (tmp.noise2d(z*multiplier, x*multiplier) + 1) **2
-            heightmap[z][x] = int(heightmap[z][x])
-
-    return heightmap
-
-
-def generate_chunk(input_heightmap, top_texture, bottom_texture, fill, z_offset=0, x_offset=0):
-    for z in range(len(input_heightmap)):  # iterate over all lists in input_heightmap
-        for x in range(len(input_heightmap[z])):  # iterate over current sublist
-            grass_generate = Voxel(pos=(x + x_offset, input_heightmap[x][z], z + z_offset), given_texture=top_texture)  # create top layer
-            if fill:
-                if input_heightmap[x][z] <= 0: continue  # check if top layer is at y<=0, if so, continue
-                while input_heightmap[x][z] >= -1:  # check if top layer is not at y=0
-                    below = Voxel(pos=(x + x_offset, input_heightmap[x][z] - 1, z + z_offset),
-                                  given_texture=bottom_texture)  # this loop makes sure there aren't any weird gaps in generation
-                    input_heightmap[x][z] -= 1
-
-def generate_tree(x, y, z):
-    trunk_bottom = Voxel(pos=(1 + x, 0 + y, z + 1), given_texture=wood_texture)
-    trunk_second = Voxel(pos=(1 + x, 1 + y, z + 1), given_texture=wood_texture)
-    trunk_third  = Voxel(pos=(1 + x, 2 + y, z + 1), given_texture=wood_texture)
-    trunk_forth  = Voxel(pos=(1 + x, 3 + y, z + 1), given_texture=wood_texture)
-    top_leaf     = Voxel(pos=(1 + x, 4 + y, z + 1), given_texture=leaves_texture)
-    left_leaf    = Voxel(pos=(0 + x, 3 + y, z + 1), given_texture=leaves_texture)
-    right_leaf   = Voxel(pos=(2 + x, 3 + y, z + 1), given_texture=leaves_texture)
-    behind_leaf  = Voxel(pos=(1 + x, 3 + y, z + 2), given_texture=leaves_texture)
-    forward_leaf = Voxel(pos=(1 + x, 3 + y, z + 0), given_texture=leaves_texture)
- 
-
-generate_chunk(create_heightmap(16, 16), grass_texture, grass_texture,  False)
-generate_tree(random.randint(1, 14), 1, random.randint(1, 14))
+terrain.generate_chunk(terrain.create_heightmap(16, 16), grass_texture, grass_texture,  False)
+terrain.generate_tree(random.randint(1, 14), 1, random.randint(1, 14))
 
 
 
